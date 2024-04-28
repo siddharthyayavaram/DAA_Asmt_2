d95938853506a3e991b23e0822eae687ffa2407b
shailx383 <shailx383@gmail.com>
2024-04-28 17:26:14 +0530

    fixed graph

--- graph.py
+++ graph.py
@@ -5,7 +5,7 @@ def plot_graph(x, y, title, x_label, y_label):
     plt.title(title)
     plt.xlabel(x_label)
     plt.ylabel(y_label)
-    plt.show()
+    # plt.show()
 
 if __name__ == "__main__":
     y = [0.01, 1.466, 11.603, 54.045,172.928, 743.773,2617.650, 6683.985]
